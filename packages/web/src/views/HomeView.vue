0f0e2972dfe0854bbdddb6716a6d7b0c1ae79664
nattoujam <Public.kyuuanago@gmail.com>
2026-04-22 01:20:39 +0900

    feat: 認証の追加

--- packages/web/src/views/HomeView.vue
+++ packages/web/src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function handleLogin() {
+  const rd = encodeURIComponent(window.location.origin)
+  window.location.href = `${import.meta.env.VITE_AUTH_URL}/?rd=${rd}`
+}
+</script>
 
 <template>
-  <ul>
-    <li>
-      <RouterLink class="is-size-4" to="/sku/list">List</RouterLink>
-    </li>
-  </ul>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Jam Shelf</h1>
+      <div class="buttons">
+        <RouterLink class="button is-light" to="/sku/list">List</RouterLink>
+        <button class="button is-primary" @click="handleLogin">
+          <span class="icon">
+            <i class="fas fa-shield-halved"></i>
+          </span>
+          <span>Authelia でログイン</span>
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
